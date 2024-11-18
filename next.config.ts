ae9c510cf7a3ff554d3383298b450fd23a483401
Nurio34 <nurioonsoftware@gmail.com>
2024-11-19 00:32:41 +0300

    Fix_Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     images: {
-        domains: ["lh3.googleusercontent.com", "localhost"],
+        domains: [
+            "lh3.googleusercontent.com",
+            "localhost",
+            "whatsup-o9bwr44yl-nurio34s-projects.vercel.app",
+        ],
     },
 };
 
