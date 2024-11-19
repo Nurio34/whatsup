ca8d396e0aaad3e93d65805613d8ade3df92e6c4
nurio34 <nurioonsoftware@gmail.com>
2024-11-19 18:49:14 +0300

    DeletePWAConfig

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
-import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
-    reactStrictMode: true,
-
+    /* config options here */
     images: {
         domains: [
             "lh3.googleusercontent.com",
@@ -14,12 +12,4 @@ const nextConfig: NextConfig = {
     },
 };
 
-export default withPWA({
-    ...nextConfig,
-    pwa: {
-        dest: "public", // The output directory for the service worker and manifest
-        disable: process.env.NODE_ENV === "development", // Disable PWA in development
-        register: true, // Registers the service worker automatically
-        skipWaiting: true, // Activate the service worker immediately after installing
-    },
-});
+export default nextConfig;
