d8d5638a694357891951a2e4a9030d6dc7dd68ee
Nurio34 <nurioonsoftware@gmail.com>
2024-11-21 03:22:58 +0300

    Check_PWA_codebase_update

--- src/app/components/Header/components/Logo/index.tsx
+++ src/app/components/Header/components/Logo/index.tsx
@@ -25,7 +25,7 @@ function Logo() {
             >
                 {process.env.NEXT_PUBLIC_LOGO_NAME || "my app"}
             </p>
-            <p className=" text-white">Update</p>
+            <p className=" text-white">New Update</p>
         </div>
     );
 }
