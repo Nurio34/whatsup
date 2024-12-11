e8049be8151009317450a9054582029719462feb
Nurio34 <nurioonsoftware@gmail.com>
2024-12-11 03:57:13 +0300

    fix_VerifyEmail

--- src/app/(auth)/verify/Client.tsx
+++ src/app/(auth)/verify/Client.tsx
@@ -12,87 +12,83 @@ import Inputs from "./_components/Inputs";
 import { setOtpExpires, setUser } from "@/store/slices/user";
 
 function VerifyClient() {
-    const { user, otpExpires } = useAppSelector((s) => s.user);
+  const { user, otpExpires } = useAppSelector((s) => s.user);
 
-    const otpExpiresRef = otpExpires && new Date(otpExpires).getTime();
-    const time = new Date().getTime();
-    const timeDiff = otpExpiresRef && (otpExpiresRef - time) / 1000;
-    const router = useRouter();
+  const otpExpiresRef = otpExpires && new Date(otpExpires).getTime();
+  const time = new Date().getTime();
+  const timeDiff = otpExpiresRef && (otpExpiresRef - time) / 1000;
+  const router = useRouter();
 
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    //! *** PREVENT THIS ROUTE IS AUTHED ***
-    useEffect(() => {
-        if (!user || user?.isVerified) {
-            router.push("/");
-        }
-    }, [user, router]);
-    //! ************************************
-
-    const resendOtp = async () => {
-        try {
-            const response = await axiosInstance.post("/auth/resend-otp", {
-                email: user?.email,
-            });
-
-            if (response.data.status === "success") {
-                dispatch(setUser(response.data.user));
-                dispatch(setOtpExpires(response.data.otpExpires));
-                toast.success(response.data.message);
-            }
-        } catch (error) {
-            if (error instanceof AxiosError) {
-                toast.error(error.response?.data.message);
-            }
-        }
-    };
-
-    //! *** PREVENT SENDİNG RESEND OTP AFTER FİRST SİGNUP ***
-    let lastPage = sessionStorage.getItem("last-page");
-    if (lastPage) {
-        lastPage = JSON.parse(lastPage);
+  //! *** PREVENT THIS ROUTE IS AUTHED ***
+  useEffect(() => {
+    if (!user || user?.isVerified) {
+      router.push("/");
     }
-    //! *****************************************************
+  }, [user, router]);
+  //! ************************************
 
-    useEffect(() => {
-        if (user && !user.isVerified && lastPage !== "signup") {
-            resendOtp();
-        } else {
-            sessionStorage.setItem("last-page", JSON.stringify("verify"));
-        }
-    }, []);
+  const resendOtp = async () => {
+    try {
+      const response = await axiosInstance.post("/auth/resend-otp", {
+        email: user?.email,
+      });
 
-    return (
-        <>
-            {user && !user?.isVerified && (
-                <main className=" grid gap-y-[3vh]  justify-items-center py-[4vh] md:py-[12vh] px-[2vw] text-center">
-                    <div>
-                        <MdOutlineMarkEmailRead color="green" size={72} />
-                    </div>
-                    <h1 className="font-semibold text-lg">
-                        Please check your email.
-                    </h1>
-                    <p>
-                        We have sent an Otp Code to{" "}
-                        <span className="text-[green]">
-                            {pure(user?.email)}
-                        </span>
-                    </p>
-                    <Inputs timeDiff={timeDiff!} />
-                    <div className="flex items-center gap-x-[1vw]">
-                        <p>Did not get the code ?</p>
-                        <button
-                            type="button"
-                            className="text-purple-500 underline underline-offset-2"
-                            onClick={resendOtp}
-                        >
-                            Click to resend.
-                        </button>
-                    </div>
-                </main>
-            )}
-        </>
-    );
+      if (response.data.status === "success") {
+        dispatch(setUser(response.data.user));
+        dispatch(setOtpExpires(response.data.otpExpires));
+        toast.success(response.data.message);
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message);
+      }
+    }
+  };
+
+  //! *** PREVENT SENDİNG RESEND OTP AFTER FİRST SİGNUP ***
+  let lastPage = sessionStorage.getItem("last-page");
+  if (lastPage) {
+    lastPage = JSON.parse(lastPage);
+  }
+  //! *****************************************************
+
+  useEffect(() => {
+    if (user && !user.isVerified && lastPage !== "signup") {
+      resendOtp();
+    } else {
+      sessionStorage.setItem("last-page", JSON.stringify("verify"));
+    }
+  }, []);
+
+  return (
+    <>
+      {user && !user?.isVerified && (
+        <main className=" grid gap-y-[3vh]  justify-items-center py-[4vh] md:py-[12vh] px-[2vw] text-center">
+          <div>
+            <MdOutlineMarkEmailRead color="green" size={72} />
+          </div>
+          <h1 className="font-semibold text-lg">Please check your email.</h1>
+          <p>
+            We have sent an Otp Code to{" "}
+            <span className="text-[green]">{pure(user?.email)}</span>
+          </p>
+          <Inputs timeDiff={timeDiff!} />
+          <div className="flex items-center gap-x-[1vw]">
+            <p>Did not get the code ?</p>
+            <button
+              type="button"
+              className="text-purple-500 underline underline-offset-2"
+              onClick={resendOtp}
+            >
+              Click to resend.
+            </button>
+          </div>
+        </main>
+      )}
+    </>
+  );
 }
 
 export default VerifyClient;
--- src/app/(auth)/verify/_components/VerifyButton.tsx
+++ src/app/(auth)/verify/_components/VerifyButton.tsx
@@ -1,67 +1,71 @@
 import axiosInstance from "@/axios";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import { setIsLoading, setUser } from "@/store/slices/user";
+import { selectUser, setIsLoading, setUser } from "@/store/slices/user";
 import { AxiosError } from "axios";
 import { useRouter } from "next/navigation";
 import { MutableRefObject } from "react";
 import toast from "react-hot-toast";
 
 function VerifyButton({
-    ButtonElement,
-    otp,
+  ButtonElement,
+  otp,
 }: {
-    ButtonElement: MutableRefObject<HTMLButtonElement | null>;
-    otp: string[];
+  ButtonElement: MutableRefObject<HTMLButtonElement | null>;
+  otp: string[];
 }) {
-    const { isLoading } = useAppSelector((s) => s.user);
-    const dispatch = useAppDispatch();
+  const user = useAppSelector(selectUser);
+  const userId = user!.id;
 
-    const isOtpProvided = otp.every((box) => box);
+  const { isLoading } = useAppSelector((s) => s.user);
+  const dispatch = useAppDispatch();
 
-    const router = useRouter();
+  const isOtpProvided = otp.every((box) => box);
 
-    const verify = async () => {
-        dispatch(setIsLoading(true));
+  const router = useRouter();
 
-        try {
-            const response = await axiosInstance.post("/auth/verify-email", {
-                otp: otp.join(""),
-            });
+  const verify = async () => {
+    dispatch(setIsLoading(true));
 
-            if (response.data.status === "success") {
-                dispatch(setUser(response.data.user));
-                router.push("/");
-                toast.success(response.data.message);
-            }
-        } catch (error) {
-            if (error instanceof AxiosError) {
-                toast.error(error.response?.data.message);
-            }
-        } finally {
-            dispatch(setIsLoading(false));
-        }
-    };
+    try {
+      const response = await axiosInstance.post("/auth/verify-email", {
+        id: userId,
+        otp: otp.join(""),
+      });
 
-    return (
-        <button
-            ref={ButtonElement}
-            type="button"
-            className={`flex items-center justify-center gap-1 w-1/2 py-[1vh] rounded-lg capitalize font-bold tracking-widest transition-all
+      if (response.data.status === "success") {
+        dispatch(setUser(response.data.user));
+        router.push("/");
+        toast.success(response.data.message);
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        toast.error(error.response?.data.message);
+      }
+    } finally {
+      dispatch(setIsLoading(false));
+    }
+  };
+
+  return (
+    <button
+      ref={ButtonElement}
+      type="button"
+      className={`flex items-center justify-center gap-1 w-1/2 py-[1vh] rounded-lg capitalize font-bold tracking-widest transition-all
                 ${
-                    !isOtpProvided || isLoading
-                        ? "bg-gray-500 text-gray-400 cursor-none pointer-events-none"
-                        : "bg-green-500 text-white hover:bg-green-500 hover:scale-105 active:scale-95"
+                  !isOtpProvided || isLoading
+                    ? "bg-gray-500 text-gray-400 cursor-none pointer-events-none"
+                    : "bg-green-500 text-white hover:bg-green-500 hover:scale-105 active:scale-95"
                 }        
             `}
-            style={{ fontVariant: "small-caps" }}
-            onClick={verify}
-        >
-            <span>{isLoading ? "Verifing" : "Verify"}</span>
-            {isLoading && (
-                <span className="loading loading-spinner loading-md"></span>
-            )}
-        </button>
-    );
+      style={{ fontVariant: "small-caps" }}
+      onClick={verify}
+    >
+      <span>{isLoading ? "Verifing" : "Verify"}</span>
+      {isLoading && (
+        <span className="loading loading-spinner loading-md"></span>
+      )}
+    </button>
+  );
 }
 
 export default VerifyButton;
